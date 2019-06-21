b58e0bed607c92cdfac410b648d9bebd55a219ba
ozeki <hhdfgg@users.noreply.github.com>
2019-06-21 16:16:50 +0900

    Initial commit

---
+++ yoketoru-cs/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace yoketoru_cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.leftLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.timeLabel.Location = new System.Drawing.Point(12, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(114, 33);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "Time: 0";
+            // 
+            // leftLabel
+            // 
+            this.leftLabel.AutoSize = true;
+            this.leftLabel.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.leftLabel.Location = new System.Drawing.Point(702, 9);
+            this.leftLabel.Name = "leftLabel";
+            this.leftLabel.Size = new System.Drawing.Size(132, 33);
+            this.leftLabel.TabIndex = 1;
+            this.leftLabel.Text = "残り: 0個";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("MS UI Gothic", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.titleLabel.Location = new System.Drawing.Point(298, 161);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(280, 54);
+            this.titleLabel.TabIndex = 2;
+            this.titleLabel.Text = "よけとるVC#";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("MS UI Gothic", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.startButton.Location = new System.Drawing.Point(307, 335);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(271, 71);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "ゲームスタート！";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.leftLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Name = "Form1";
+            this.Text = "よけとるVC#";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label leftLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ yoketoru-cs/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yoketoru_cs
+{
+    enum SCENE
+    {
+        TITLE,
+        GAME,
+        CLEAR,
+        GAMEOVER,
+        NONE
+    }
+
+    public partial class Form1 : Form
+    {
+        const int ITEM_COUNT = 5;
+
+        SCENE nowscene;
+        SCENE nextscene;
+
+        Label[] labels = new Label[ITEM_COUNT];
+
+        int[] vx = new int[ITEM_COUNT];
+        int[] vy = new int[ITEM_COUNT];
+
+        int left;
+
+        private static Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            nextscene = SCENE.TITLE;
+            nowscene = SCENE.NONE;
+
+            for (int i = 0; i < ITEM_COUNT; i++)
+            {
+                labels[i] = new Label();
+                labels[i].AutoSize = true;
+                labels[i].Text = "★";
+                Controls.Add(labels[i]);
+                labels[i].Visible = false;
+            }
+        }
+
+        void initScene()
+        {
+            if (nextscene == SCENE.NONE) return;
+
+            nowscene = nextscene;
+            nextscene = SCENE.NONE;
+
+            switch (nowscene)
+            {
+                case SCENE.TITLE:
+                    titleLabel.Visible = true;
+                    startButton.Visible = true;
+                    break;
+                case SCENE.GAME:
+                    titleLabel.Visible = false;
+                    startButton.Visible = false;
+                    left = ITEM_COUNT;
+                    leftLabel.Text = "残り:" + left + "個";
+                    for (int i = 0; i < ITEM_COUNT; i++)
+                    {
+                        labels[i].Visible = true;
+                        labels[i].Left = rand.Next(ClientSize.Width - labels[i].Width);
+                        labels[i].Top = rand.Next(ClientSize.Height - labels[i].Height);
+                        vx[i] = rand.Next(-5, 5);
+                        vy[i] = rand.Next(-5, 5);
+                    }
+                    break;
+
+            }
+
+
+        }
+
+        void updateScene()
+        {
+            if (nowscene != SCENE.GAME) return;
+
+            for (int i = 0; i < ITEM_COUNT; i++)
+            {
+                labels[i].Left += vx[i];
+                labels[i].Top += vy[i];
+
+                if (labels[i].Left < 0)
+                {
+                    vx[i] = Math.Abs(vx[i]);
+                }
+                if (labels[i].Right > ClientSize.Width)
+                {
+                    vx[i] = -Math.Abs(vx[i]);
+                }
+                if (labels[i].Top < 0)
+                {
+                    vy[i] = Math.Abs(vy[i]);
+                }
+                if (labels[i].Bottom > ClientSize.Height)
+                {
+                    vy[i] = -Math.Abs(vy[i]);
+                }
+
+                Point mp = PointToClient(MousePosition);
+                if (
+                      (mp.X >= labels[i].Left)
+                   && (mp.X <= labels[i].Right)
+                   && (mp.Y >= labels[i].Top)
+                   && (mp.Y <= labels[i].Bottom)
+                    )
+                {
+                    labels[i].Visible = false;
+                    left--;
+                    leftLabel.Text = "残り:" + left + "個";
+                }
+            }
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            nextscene = SCENE.GAME;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            initScene();
+            updateScene();
+        }
+    }
+}
